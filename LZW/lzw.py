1e8ecdd9b401d744cbc84661c79ce71db4fb63fa
Dalius <dalius.b2011@gmail.com>
2022-01-20 17:14:37 +0200

    updated parsing
    updated parsing

--- LZW/lzw.py
+++ LZW/lzw.py
@@ -1,29 +1,43 @@
+from ast import parse
 import os
 import sys
 import time
+import argparse
 
 from decoder import LZWDecoding
 from encoder import LZWEncoding
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--path', help='path of the file used', required=True)
+parser.add_argument('--bits', help='bits used to encode one word' , required=True)
+args = parser.parse_args()
+
+path = args.path
+bits = int(args.bits)
+
 if __name__ == "__main__":
-    if sys.argv[1] == 'c' and len(sys.argv) == 4 and 8 <= int(sys.argv[3]) <= 15:
-        start_time = time.time()
-        encoding = LZWEncoding(path=sys.argv[2], param=int(sys.argv[3]))
-        encoded_path = encoding.lzw_compress()
-        print(f'Compressed to {encoded_path}')
-        print(f'Before compress: {os.path.getsize(sys.argv[2])}')
-        print(f'After compress:  {os.path.getsize(encoded_path)}')
-        print(f"--- {time.time() - start_time:.3f} seconds ---")
-    elif sys.argv[1] == 'd' and len(sys.argv) == 4:
-        start_time = time.time()
-        encoded_file = sys.argv[2]
-        decoded_file = sys.argv[3]
-        decoding = LZWDecoding(encoded_file)
-        decoding.lzw_decompress(decoded_file)
-        print(f'Decompressed to {decoded_file}')
-        print(f'Before decompress:     {os.path.getsize(encoded_file)}')
-        print(f'After Decompressed:  {os.path.getsize(decoded_file)}')
-        print(f"--- {time.time() - start_time:.3f} seconds ---")
-    else:
-        sys.exit("Invalid arguments. Format [c|d] [inputFile.*] [|outputFile.*] [k| ]")
+    if bits < 9 or bits > 15:
+        sys.exit('Library size is set incorrectly, bits should be between 9 and 15')
+
+    filename, file_extension = os.path.splitext(path)
+
+    # encoding
+    start_time = time.time()
+    encoding = LZWEncoding(path, bits)
+    encoded_path = encoding.lzw_compress()
+    print(f'Compressed to {encoded_path}')
+    print(f'Before compress: {os.path.getsize(path)}')
+    print(f'After compress:  {os.path.getsize(encoded_path)}')
+    print(f"--- {time.time() - start_time:.3f} seconds ---")
+
+    #decoding
+    start_time = time.time()
+    encoded_file = f'{filename}.lzw'
+    decoded_file = f'{filename}.lzwdecoded'
+    decoding = LZWDecoding(encoded_file)
+    decoding.lzw_decompress(decoded_file)
+    print(f'Decompressed to {decoded_file}')
+    print(f'Before decompress:     {os.path.getsize(encoded_file)}')
+    print(f'After Decompressed:  {os.path.getsize(decoded_file)}')
+    print(f"--- {time.time() - start_time:.3f} seconds ---")
